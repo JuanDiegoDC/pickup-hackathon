b44377f92acfa1679701e999ae66e6e29b5e735e
Tim Chang <tc932@drexel.edu>
2018-08-03 14:51:30 -0700

    small changes in appjs

--- pickup2/App.js
+++ pickup2/App.js
@@ -197,16 +197,6 @@ export default StackNavigator({
   }
 }, {initialRouteName: 'Login'});
 
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>Open up App.js to start working on your app!</Text>
-//       </View>
-//     );
-//   }
-// }
-
 
 const styles = StyleSheet.create({
   container: {
