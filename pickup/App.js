e34e9ff27d23ae0c00f851cc70a3a90ead6c9f08
Tim Chang <tc932@drexel.edu>
2018-08-03 11:56:49 -0700

    connected mongo, added models, added envsh installed packages

--- pickup/App.js
+++ pickup/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+const Player = require('./models').Player;
 
 export default class App extends React.Component {
   render() {
