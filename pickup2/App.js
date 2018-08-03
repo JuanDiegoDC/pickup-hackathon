b07f4080c1b7d05a45b5529b7515c6e017ce6cf3
Juan Diego Donneys <juandiego@Juans-MacBook-Pro.local>
2018-08-03 14:13:51 -0700

    added comma :)

--- pickup2/App.js
+++ pickup2/App.js
@@ -103,7 +103,7 @@ class RegisterScreen extends React.Component {
           name: '',
           age: '',
           position: '',
-          skill: ''
+          skill: '',
           imgUrl: ''
         })
       } else {
