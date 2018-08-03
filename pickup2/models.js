66bfbe7195293da36d36f85ec1bb45f9dfaf6e8d
Asheesh Chopra <aschopra@usc.edu>
2018-08-03 12:54:52 -0700

    Models Schema pushed to pickup2

--- pickup2/models.js
+++ pickup2/models.js
@@ -4,10 +4,16 @@ let Schema = mongoose.Schema;
 //contact model
 
 let PlayerSchema = new Schema({
+    username: String,
+    password: String,
     name: String,
     age: Number,
     position: String,
     skill: String,
+    imgUrl: {
+      type: String,
+      default: "https://horizons-static.s3.amazonaws.com/horizons_h.png"
+    }
 });
 
 let Player =  mongoose.model('Player', PlayerSchema)
@@ -32,7 +38,7 @@ let CourtSchema = new Schema({
     type: [],
     ref: "Game"
   },
-  location: 
+  location:
 
 })
 
