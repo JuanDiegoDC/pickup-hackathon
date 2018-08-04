b7267f8ec2e6a7de47920dd9a807e15942a4ae36
Tim Chang <tc932@drexel.edu>
2018-08-03 17:27:49 -0700

    overhauled initial landing page

--- pickup2/App.js
+++ pickup2/App.js
@@ -9,7 +9,8 @@ import {
   Alert,
   AppRegistry,
   Image,
-  Button
+  Button,
+  ImageBackground
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 // import { Button } from 'react-native-elements';
@@ -36,15 +37,34 @@ class LoginScreen extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.textBig}>Login to Pickup!</Text>
-        <TouchableOpacity onPress={ () => {this.press()} } style={[styles.button, styles.buttonGreen]}>
-          <Text style={styles.buttonLabel}>Tap to Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.button, styles.buttonBlue]} onPress={ () => {this.register()} }>
-          <Text style={styles.buttonLabel}>Tap to Register</Text>
-        </TouchableOpacity>
+      // <View style={styles.container}>
+      //   <Text style={[styles.textBig, styles.textColor]}>PICKUP</Text>
+      //   <TouchableOpacity onPress={ () => {this.press()} } style={[styles.button, styles.buttonGreen]}>
+      //     <Text style={styles.buttonLabel}>Tap to Login</Text>
+      //   </TouchableOpacity>
+      //   <TouchableOpacity style={[styles.button, styles.buttonBlue]} onPress={ () => {this.register()} }>
+      //     <Text style={styles.buttonLabel}>Tap to Register</Text>
+      //   </TouchableOpacity>
+      // </View>
+      <View style={styles.container1}>
+        <ImageBackground source={require('./assets/architecture.jpg')} style={styles.backgroundImage}>
+          <View style={styles.content}>
+            <Text style={styles.logo}>PICKUP!</Text>
+            <View style={styles.inputContainer}>
+              {/* <TextInput underlineColor='transparent' style={styles.input} placeholder='username'>
+              </TextInput>
+              <TextInput secureTextEntry={true} underlineColor='transparent' style={styles.input} placeholder='password'>
+              </TextInput> */}
+              <TouchableOpacity onPress={() => {this.press()} } style={styles.buttonContainer}>
+                <Text style={styles.buttonText}>LOGIN</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.buttonContainer} onPress={ () => {this.register()} }>
+                <Text style={styles.buttonText}>REGISTER</Text>
+              </TouchableOpacity>
+          </View>
       </View>
+    </ImageBackground>
+    </View>
     )
   }
 }
@@ -252,24 +272,30 @@ class Login extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Image
+          style={{width: 150, height: 150, marginBottom: 20}}
+          source={require('./mjtransparent.png')}
+        />
         <View style={{ borderRadius: 4, borderWidth: 0.5, borderColor: 'black', width: 300, marginBottom: 10}}>
           <TextInput
-            style={{height: 40}}
+            style={{height: 40, width: 300, borderColor: 'white', borderWidth: 2, color: 'white'}}
             placeholder="Username"
+            placeholderTextColor='white'
             onChangeText={(text) => this.setState({username: text})}
             value={this.state.username}
           />
         </View>
         <View style={{ borderRadius: 4, borderWidth: 0.5, borderColor: 'black', width: 300, marginBottom: 10}}>
           <TextInput
-            style={{height: 40}}
+            style={{height: 40, width: 300, borderColor: 'white', borderWidth: 2, color: 'white'}}
             placeholder="Password"
+            placeholderTextColor='white'
             secureTextEntry={true}
             onChangeText={(text) => this.setState({password: text})}
             value = {this.state.password}
           />
         </View>
-        <View style={{backgroundColor: '#FFC0CB', borderRadius: 4, borderWidth: 0.5}}>
+        <View style={{backgroundColor: '#ff9933', borderRadius: 4, borderWidth: 0.5}}>
           <TouchableOpacity onPress={(e) => this.handleSubmit(e)}>
             <View style={{justifyContent: 'center', alignItems: 'center'}}>
               <Text style={{color: 'white', height: 40, width: 300, fontSize: 30, textAlign:'center'}}>Login</Text>
@@ -406,5 +432,61 @@ const styles = StyleSheet.create({
     alignSelf: 'stretch',
     borderColor: 'black',
     padding: 5
+  },
+  textColor: {
+    color: 'white'
+  },
+  backgroundImage: {
+    flex: 1,
+    alignSelf: 'stretch',
+    width: null,
+    justifyContent: 'center'
+  },
+  content: {
+    alignItems: 'center'
+  },
+  logo: {
+    color: 'white',
+    fontSize: 40,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    textShadowColor: "#252525",
+    textShadowOffset: {width: 2, height: 2},
+    textShadowRadius: 15,
+    marginBottom: 20
+  },
+  inputContainer: {
+    margin: 20,
+    marginBottom: 0,
+    padding: 20,
+    paddingBottom: 10,
+    alignSelf: 'stretch',
+    borderWidth: 1,
+    borderColor: '#fff',
+    backgroundColor: 'rgba(255,255,255,0.2)'
+  },
+  input: {
+    fontSize: 16,
+    height: 40,
+    padding: 10,
+    marginBottom: 10,
+    backgroundColor: 'rgba(255,255,255,1)'
+  },
+  buttonContainer: {
+    alignSelf: 'stretch',
+    margin: 20,
+    padding: 20,
+    backgroundColor: 'blue',
+    borderWidth: 1,
+    borderColor: '#fff',
+    backgroundColor: 'rgba(255,255,255,0.6)'
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  container1: {
+    flex: 1
   }
 });
